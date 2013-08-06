ab6cb9ff157a41841824dc1e845eccb357ed0f2a
David Corking <dmc-source@dcorking.com>
2013-08-06 08:53:45 +0100

    Added fix-agent and prompt-vars.sh

---
+++ prompt-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# prompt-vars.sh
+#
+# source prompt-vars.sh 
+# to customize the prompt variables in your Bash shell 
+
+# Adapted from various prompts by David Corking, 2013
+
+source ./git-prompt.sh
+
+export PROMPT_COMMAND='__git_ps1 "${debian_chroot:+($debian_chroot)}\u@\h:\w" "\\\$ "'
